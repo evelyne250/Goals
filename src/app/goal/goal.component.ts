b6f844b9ba5ddbc2ad132b45f20489281421d963
pascaline <passyish5@gmail.com>
2019-08-28 09:51:15 +0200

    new

---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
+import { HttpClient } from '@angular/common/http';
+import { Quote } from '../quote-class/quote';
+import { Router } from '@angular/router';
+import{QuoteRequestService} from '../quote-http/quote-request.service'
+
+@Component({
+ selector: 'app-goal',
+ templateUrl: './goal.component.html',
+ styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+  
+
+ goals:Goal[];
+ alertService:AlertService;
+ quote:Quote;
+//  router:Router;
+//  quoteService:QuoteRequestService;
+ toggleDetails(index){
+   this.goals[index].showDescription = !this.goals[index].showDescription;
+ }
+ goToUrl(id){
+  this.router.navigate(['/goals',id])
+}
+ deleteGoal(isComplete, index){
+   if (isComplete) {
+     let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`);
+     if (toDelete){
+  
+        this.goals.splice(index,1)
+        this.alertService.alertMe("The goal has been deleted")
+     }
+   }
+ }
+addNewGoal(goal){
+   let goalLength = this.goals.length;
+   goal.id = goalLength+1;
+   goal.completeDate = new Date(goal.completeDate)
+   this.goals.push(goal)
+ }
+ constructor(goalService:GoalService, alertService:AlertService, private http:HttpClient, private quoteService:QuoteRequestService, private router:Router) {
+   this.goals = goalService.getGoals()
+   this.alertService = alertService;
+  //  this.quoteService=quoteService;
+
+ }
+ ngOnInit() {
+
+  interface ApiResponse{
+    author:string;
+    quote:string;
+  }
+
+  this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+    // Succesful API request
+    this.quote = new Quote(data.author, data.quote)
+  },err=>{
+    this.quote = new Quote("Winston Churchill","Never never give up!")
+    console.log("An error occurred")
+})
+}
+
+}
